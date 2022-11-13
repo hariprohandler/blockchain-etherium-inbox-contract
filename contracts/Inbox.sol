c19a7379db77d1802a94a4815eba625deeaf7d18
Hari Prasad Sharma <hari.s@rocketfuelblockchain.com>
2022-11-13 21:30:35 +0530

    Test cases

--- contracts/Inbox.sol
+++ contracts/Inbox.sol
@@ -2,19 +2,12 @@ pragma solidity ^0.4.17;
 
 contract Inbox {
     string public message;
-
-    constructor(string initialMessage) public {
+    
+    function Inbox(string initialMessage) public {
         message = initialMessage;
     }
-
+    
     function setMessage(string newMessage) public {
         message = newMessage;
-    } 
-
-    function doMath(int a, int b) private{
-        a+b;
-        b-a;
-        a*b;
-        a == 0;
     }
 }
