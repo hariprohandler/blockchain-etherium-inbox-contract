c39bcfd13f5b41a2aff730fae29ec00ab91c1442
Hari Prasad Sharma <hari.s@rocketfuelblockchain.com>
2022-11-17 10:45:50 +0530

    Inbox contract

---
+++ deploy.js
@@ -0,0 +1,25 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const Web3 = require('web3');
+
+const { abi, evm } = require('./compile');
+
+provider = new HDWalletProvider(
+  'Quick brown fox jumps over little lazy dog Hari is the best',
+  'https://goerli.infura.io/v3/8ed24f27294f4ed4a7c2357557e616af'
+);
+
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+
+  console.log('Attempting to deploy from account', accounts[0]);
+
+  const result = await new web3.eth.Contract(abi)
+    .deploy({ data: evm.bytecode.object, arguments: ['Hi there!'] })
+    .send({ gas: '1000000', from: accounts[0] });
+
+  console.log('Contract deployed to', result.options.address);
+  provider.engine.stop();
+};
+deploy();
